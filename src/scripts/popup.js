5fbae3e69669eb2409077a5e878926ad01bc4f36
Leo Orellana <leo@camba.coop>
2019-03-19 16:30:52 -0300

    add scan function

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,9 +1,11 @@
 var scanButton = document.getElementById('scanButton');
-var isActive = false;
+// var isActive = false;
 const toggleButton = require('./utils/toggleButton.js');
+// const scanner = require('./scanner')
 
 toggleButton(scanButton);
 scanButton.addEventListener('click', (e) => {
   toggleButton(scanButton);
   // script de scaneo
+  // scanner.scan()
 });
